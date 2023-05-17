3bdff73444f0834ddca47ff89d8e9ccbe5970f93
muhameddadel <muhameddadel2000@gmail.com>
2023-05-17 17:36:39 +0300

    forms

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SECRET_KEY'] = '26d1bd3fcee5a4df129522e7'
 db = SQLAlchemy(app)
 
 with app.app_context():
