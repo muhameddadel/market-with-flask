8f3310cb73264a7df567870851741ecdcf5aaf9b
muhameddadel <muhameddadel2000@gmail.com>
2023-05-18 20:28:20 +0300

    form validation

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
-from flask import render_template
-from app.models import Item
+from flask import render_template, redirect, url_for
+from app.models import Item, User
 from app.forms import RegisterForm
+from app import db
+
 
 @app.route('/')
 @app.route('/home')
@@ -14,7 +16,17 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', method=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                            email_address=form.email_address.data,
+                            password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(err_msg)
     return render_template('register.html', form=form)
